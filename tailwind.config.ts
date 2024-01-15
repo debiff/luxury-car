4aa9e8e987250f3db6f602f4013a274a454c74e7
Simone <simone.biffi.91@gmail.com>
2024-01-15 22:46:47 +0100

    feat: add menu and slider

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,33 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+const config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
+        "13": "repeat(13, minmax(0, 1fr))"
+      },
+      backgroundColor: {
+        blue: {
+          400: "#0C1315"
+        }
       },
       colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
-        },
-      },
+        gray: {
+          400: "#404040"
+        }
+      }
     },
     keyframes: {
       shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
-        },
-      },
-    },
+        "100%": {
+          transform: "translateX(100%)"
+        }
+      }
+    }
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")]
 };
 export default config;
