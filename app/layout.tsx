494720a7d064d979bd9b868fe0b37e1fb90bab2b
Simone <simone.biffi.91@gmail.com>
2024-01-16 17:33:58 +0100

    try to fix slider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
+// "use client";
 import "@/app/ui/global.css";
-import "swiper/css";
-import "swiper/css/autoplay";
-import "swiper/css/effect-fade";
 import { sora } from "@/app/ui/fonts";
 import { Navbar } from "@/app/ui/dashboard/navbar";
 import { Slider } from "@/app/ui/dashboard/slider";
@@ -27,7 +24,6 @@ export default function RootLayout({
         <Navbar />
         <Slider />
         {children}
-        <SpeedInsights />
       </body>
     </html>
   );
