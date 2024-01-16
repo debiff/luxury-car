08945d5f61408bb4d1e25b744ff6cd8d9f28c7f2
Simone <simone.biffi.91@gmail.com>
2024-01-16 17:02:24 +0100

    try to fix slider

--- app/ui/dashboard/slider/index.tsx
+++ app/ui/dashboard/slider/index.tsx
@@ -1,7 +1,4 @@
 "use client";
-import "swiper/css";
-import "swiper/css/autoplay";
-import "swiper/css/effect-fade";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade } from "swiper/modules";
 import Image from "next/image";
