0391d3d3ce43ad6a1a258e0c67c0338bd75325a3
Simone <simone.biffi.91@gmail.com>
2024-03-03 22:11:15 +0100

    fix fleet desktop page

--- app/fleet/page.tsx
+++ app/fleet/page.tsx
@@ -1,12 +1,58 @@
 "use client";
 import Image from "next/image";
-import { elegantIcons, epilogue, inconsolata, sora } from "@/app/ui/fonts";
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { epilogue, sora } from "@/app/ui/fonts";
 import { SelectBox } from "@/app/ui/commons/SelectBox";
-import { Cars } from "@/app/lib/placeholder-car";
+import { Cars, Car as CarType } from "@/app/lib/placeholder-car";
 import { Button } from "@/app/ui/commons/Button";
 
+const Car = (car: CarType) => (
+  <div key={car.id} className={"pt-8 px-8 pb-10 w-full min-h-1 bg-[#f7f5f2]"}>
+    <Image
+      className={"w-full"}
+      src={car.image}
+      alt={car.name}
+      width={car.width}
+      height={car.height}
+    />
+    <div
+      className={`${epilogue.className} mt-9 flex flex-col justify-center text-[#0c1315]`}
+    >
+      <h5 className={"mb-8"}>{car.name}</h5>
+      <span className={`${sora.className} mb-4 text-[16px] text-[#404040]`}>
+        {car.descriptionShort}
+      </span>
+      <div className={`${sora.className} flex items-end`}>
+        <span
+          className={` 
+                        inline-flex 
+                        text-4xl 
+                        leading-none`}
+        >
+          <span
+            className={`
+                        self-start
+                        pt-1
+                        text-xl
+                        leading-none
+                        `}
+          >
+            $
+          </span>
+          <span>180</span>
+        </span>
+        <span
+          className={`
+                    before:content-['/']
+                    before:mx-1
+                    `}
+        >
+          hour
+        </span>
+      </div>
+      <Button variant={"secondary"}>MORE DETAILS</Button>
+    </div>
+  </div>
+);
 const Page = () => {
   return (
     <main className="flex min-h-screen flex-col bg-white">
@@ -63,62 +109,19 @@ const Page = () => {
           </div>
         </div>
       </section>
-      <section className={"w-full px-10 pb-32 flex flex-col gap-10"}>
-        {Cars.map(car => {
-          return (
-            <div
-              key={car.id}
-              className={"pt-8 px-8 pb-10 w-full min-h-1 bg-[#f7f5f2]"}
-            >
-              <Image
-                className={"w-full"}
-                src={car.image}
-                alt={car.name}
-                width={car.width}
-                height={car.height}
-              />
-              <div
-                className={`${epilogue.className} mt-9 flex flex-col justify-center text-[#0c1315]`}
-              >
-                <h5 className={"mb-8"}>{car.name}</h5>
-                <span
-                  className={`${sora.className} mb-4 text-[16px] text-[#404040]`}
-                >
-                  {car.descriptionShort}
-                </span>
-                <div className={`${sora.className} flex items-end`}>
-                  <span
-                    className={` 
-                        inline-flex 
-                        text-4xl 
-                        leading-none`}
-                  >
-                    <span
-                      className={`
-                        self-start
-                        pt-1
-                        text-xl
-                        leading-none
-                        `}
-                    >
-                      $
-                    </span>
-                    <span>180</span>
-                  </span>
-                  <span
-                    className={`
-                    before:content-['/']
-                    before:mx-1
-                    `}
-                  >
-                    hour
-                  </span>
-                </div>
-                <Button variant={"secondary"}>MORE DETAILS</Button>
-              </div>
-            </div>
-          );
-        })}
+      <section className={"w-full px-10 pb-32 flex flex-col gap-10 md:hidden"}>
+        {Cars.map(car => (
+          <Car key={car.id} {...car} />
+        ))}
+      </section>
+      <section
+        className={
+          "relative md:grid grid-cols-4 rw-full px-10 pb-32 gap-10 hidden"
+        }
+      >
+        {Cars.map(car => (
+          <Car key={car.id} {...car} />
+        ))}
       </section>
     </main>
   );
