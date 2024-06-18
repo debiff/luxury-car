aa7032fca95d48d8a71e1dbb6cd77efffc3871b7
debiff <simone.biffi.91@gmail.com>
2024-06-18 22:27:24 +0200

    fix

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -25,12 +25,12 @@ export async function POST(request: NextRequest) {
     }
   });
 
-  console.log(
-    process.env.EMAIL_USER,
-    process.env.EMAIL_PASSWORD,
-    process.env.EMAIL_BOOKING_FROM,
-    process.env.EMAIL_BOOKING_TO
-  );
+  return NextResponse.json({
+    message: `${process.env.EMAIL_USER},
+    ${process.env.EMAIL_PASSWORD},
+    ${process.env.EMAIL_BOOKING_FROM},
+    ${process.env.EMAIL_BOOKING_TO}`
+  });
 
   const mailOptions: Mail.Options = {
     from: process.env.EMAIL_BOOKING_FROM,
